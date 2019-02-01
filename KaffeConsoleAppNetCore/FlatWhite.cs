a0210179a01f5169ae3270711e2388622d074448
Jacob Larsen <jacob.larsen6401@hotmail.dk>
2019-02-01 13:47:36 +0100

    Mange ting

---
+++ KaffeConsoleAppNetCore/FlatWhite.cs
@@ -0,0 +1,33 @@
+﻿using System.Buffers.Text;
+using System.Runtime.CompilerServices;
+using kaffe;
+
+namespace KaffeConsoleAppNetCore
+{
+    public class FlatWhite : Kaffe, Imælk
+    {
+        /// <summary>
+        /// Hvor meget mælk skal bruges til Latte
+        /// </summary>
+        /// <returns></returns>
+        public int MlMælk()
+        {
+            return 160;
+        }
+
+        public override int Pris()
+        {
+            return 45;
+        }
+
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+
+        public FlatWhite(int rabat) : base(rabat)
+        {
+        }
+
+    }
+}
